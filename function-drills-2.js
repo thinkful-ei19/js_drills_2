dcfd1821a0dbdff59fcaca4c70b92699cf6463ed
Alexa Scott <arscott4792@gmail.com>
2018-02-19 15:44:02 -0800

    completed beyond function drill

--- function-drills-2.js
+++ function-drills-2.js
@@ -1,5 +1,23 @@
-function jediName(firstName, lastName){
+/* function jediName(firstName, lastName){
     return lastName.slice(0,3).concat(firstName.slice(0,2));
 }
 
 jediName('George', 'Colon');
+*/
+
+function beyond(num) {
+  if(num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
+    console.log('And beyond');
+  } else if(isFinite(num) && num > 0) {
+    console.log('To Infinity');
+  } else if(isFinite(num) && num < 0) {
+    console.log('To Negative Infinity');
+  } else if(num === 0) {
+    console.log('Staying Home');
+  }
+}
+
+beyond(10 / 0);
+beyond(0);
+beyond(10);
+beyond(-10);
