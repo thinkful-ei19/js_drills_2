4ac2c0907937214ed14433346b0d704500e5a42d
George Colon <geocolon@gmail.com>
2018-02-19 15:24:44 -0800

    adding jedi function

--- function-drills-2.js
+++ function-drills-2.js
@@ -1,3 +1,5 @@
-functionj jediName(firstName, lastName){
-    
+function jediName(firstName, lastName){
+    return lastName.slice(0,3).concat(firstName.slice(0,2));
 }
+
+jediName('George', 'Colon');
